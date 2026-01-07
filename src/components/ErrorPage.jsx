610a6b86e455faadc087f12b1f3eebb9031cd9ac
Samm005 <samudyathakaranth@gmail.com>
2026-01-07 22:53:56 +0530

    Added error page functionality and styled error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,17 @@
 import { useRouteError } from "react-router-dom";
+import "./errorPage.css";
 
 function ErrorPage() {
   const err = useRouteError();
+  console.log(err);
   return (
-    <>
-      <h1>Looks like you entered a wrong link</h1>
-    </>
+    <div className="err-data">
+      <p id="error">{err.data}</p>
+      <p id="status">
+        {err.status} {err.statusText}
+      </p>
+      <h2 id="final">Please enter the proper destination...</h2>
+    </div>
   );
 }
 
