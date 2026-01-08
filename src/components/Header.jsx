83282799a1f87baa19d8cb9bd73fce6e3f81761b
Samm005 <samudyathakaranth@gmail.com>
2026-01-08 20:21:48 +0530

    Coded Home page with styling done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <></>;
+  return (
+    <>
+      <Link to="/">
+        <li>Home</li>
+      </Link>
+      <Link to="/books/:category">
+        <li>Browse Books</li>
+      </Link>
+      <Link to="/addbook">
+        <li>Add Book</li>
+      </Link>
+    </>
+  );
 }
 
 export default Header;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,55 @@
-import Header from "./Header";
+import { Link } from "react-router-dom";
+import { booksByCategory, categories } from "../utils/books.js";
+import "./home.css";
 
 function Home() {
+  const allBooks = Object.values(booksByCategory).flat();
+  const popularBooks = allBooks.filter((book) => book.isPopular);
+
   return (
-    <>
-      <h1 className="welcome-msg">Welcome to Online Library System</h1>
-    </>
+    <div className="container">
+      <div className="welcome-msg">
+        <h1>Welcome to Online Library System</h1>
+        <p className="welcome">
+          find your favorite books in different categories....📚📚
+        </p>
+      </div>
+      <section className="welcome">
+        Categories
+        <ul className="category-list">
+          {categories.map((cat) => (
+            <li key={cat.id}>
+              <Link to={`/books/${cat.category}`} className="category-item">
+                {cat.category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <h1 id="Popular">Popular Books</h1>
+      <section className="book-card">
+        {popularBooks.map((book) => (
+          <div key={book.id} className="book">
+            <img
+              src={book.image}
+              alt={book.title}
+              width="200px"
+              height="200px"
+              className="img"
+            />
+            <p className="popular-text">{book.title}</p>
+            <p className="popular-text">{book.author}</p>
+            <p className="popular-text">Pages: {book.pages}</p>
+            <p className="popular-text">
+              Year of Publishment: {book.publishedYear}
+            </p>
+            <Link to={`/book/${book.id}`} id="view-details">
+              View Details
+            </Link>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
 
