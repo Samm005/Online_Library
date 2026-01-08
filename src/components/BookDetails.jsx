e838ac45789c173a20d577363da5f10958f7cccf
Samm005 <samudyathakaranth@gmail.com>
2026-01-09 01:36:34 +0530

    Added Book details functionality that shows a specific book on pressing view details in home page or browse books page(Not added styling yet)

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,5 +1,28 @@
+import { useParams } from "react-router-dom";
+import { booksByCategory } from "../utils/books";
+import "./BookDetails.css";
+
 function BookDetails() {
-  return <></>;
+  const { id } = useParams();
+  const allBooks = Object.values(booksByCategory).flat();
+  const book = allBooks.find((b) => b.id == id);
+
+  if (!book) {
+    return <h1>No Books Found</h1>;
+  }
+
+  return (
+    <div className="bookdetails">
+      <img src={book.image} alt={book.title} />
+      <p>{book.title}</p>
+      <p>Author:{book.author}</p>
+      <p>Pages: {book.pages}</p>
+      <p>Year : {book.publishedYear}</p>
+      <p>Lang: {book.language}</p>
+      <p>Rating: {book.rating}</p>
+      <p>{book.description}</p>
+    </div>
+  );
 }
 
 export default BookDetails;
